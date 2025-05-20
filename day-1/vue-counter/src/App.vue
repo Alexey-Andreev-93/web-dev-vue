9a1aa05135e3c1243a2e553b4e502b973c8a139f
Alexey-Andreev-93 <lexandreeev@yandex.ru>
2025-05-20 20:06:14 +0300

    День 1: Счетчик на Vue 3

---
+++ day-1/vue-counter/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1 :class="{'red-text' : count >= 10}">Счётчик: {{ count }}</h1>
+    <button @click="count++">+1</button>
+    <button @click="count--">-1</button>
+    <button @click="count += 10">+10</button>
+    <button @click="count = 0">Сброс</button>
+  </div>
+</template>
